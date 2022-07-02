102a7dbddc3d7a194240091f59e4d6c8a7450845
gana2003 <meenakshigana0@gmail.com>
2022-07-02 15:57:35 +0530

    receipee completed

---
+++ src/app/receipee/receipee.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-receipee',
+  templateUrl: './receipee.component.html',
+  styleUrls: ['./receipee.component.css']
+})
+export class ReceipeeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
